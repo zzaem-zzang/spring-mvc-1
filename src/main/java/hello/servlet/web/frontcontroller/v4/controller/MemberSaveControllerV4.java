e9336169d7ae558e2d80b88379a7cf1ddec719a4
육재민 <rjsekawoals@naver.com>
2026-03-07 20:15:29 +0900

    feat: 뷰 경로 중복 제거

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+public class MemberSaveControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> parseMap, Map<String, Object> model) {
+        String username = parseMap.get("username");
+        int age = Integer.parseInt(parseMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.put("member",member);
+        return "save-result";
+    }
+}
